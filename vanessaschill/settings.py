d23084ed1116a688dcfc0cac6657e0f87a21f9cc
rpschill <rpschill@gmail.com>
2017-07-07 14:38:14 -0400

    updating allowed hosts setting

--- vanessaschill/settings.py
+++ vanessaschill/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vschill.herokuapp.com']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
