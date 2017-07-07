b43b20a14bac726ec5a39458937b96434191819a
rpschill <rpschill@gmail.com>
2017-07-07 15:40:35 -0400

    update

--- vanessaschill/settings.py
+++ vanessaschill/settings.py
@@ -85,9 +85,11 @@ WSGI_APPLICATION = 'vanessaschill.wsgi.application'
 
 import dj_database_url
 
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 
 # Password validation
