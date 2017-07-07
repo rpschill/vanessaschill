c808f1a9f4b201e1464ea1e6beaf9dd00daa7924
rpschill <rpschill@gmail.com>
2017-07-07 15:42:34 -0400

    missing comma in tuple

--- vanessaschill/settings.py
+++ vanessaschill/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static')
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 # Whitenoise setup for serving static files in production
