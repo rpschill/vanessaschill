2400e1c9a57145d24240f111484d425e4737c37e
rpschill <rpschill@gmail.com>
2017-07-13 14:27:10 -0400

    adding back db

--- vanessaschill/settings.py
+++ vanessaschill/settings.py
@@ -83,11 +83,12 @@ WSGI_APPLICATION = 'vanessaschill.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
