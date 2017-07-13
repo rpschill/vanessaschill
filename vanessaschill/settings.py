cc95b300afe32b32074e3822c40eb994a1db2e90
rpschill <rpschill@gmail.com>
2017-07-13 14:23:11 -0400

    removing db

--- vanessaschill/settings.py
+++ vanessaschill/settings.py
@@ -83,12 +83,11 @@ WSGI_APPLICATION = 'vanessaschill.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
